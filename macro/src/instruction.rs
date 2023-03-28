40f48ea06253a6e03f86afad91e2cb4f7187526a
jovan.pavlovic99@gmail.com <jovan.pavlovic99@gmail.com>
2023-03-28 17:57:47 +0200

    push

--- macro/src/instruction.rs
+++ macro/src/instruction.rs
@@ -1,14 +1,30 @@
+use std::marker::PhantomData;
+
 use iced_x86::code_asm::CodeAssembler;
 use proc_macro2::Ident;
 use syn::parse::{Parse, ParseStream, Result};
 
+use crate::{INTEL, ATNT};
+
+
+
 #[derive(Debug)]
-pub enum Instruction
+pub enum Instruction<T>
 {
     NoOperand(Ident),
+
+    _Phantom(PhantomData<T>)
 }
 
-impl Parse for Instruction {
+
+impl Parse for Instruction<INTEL> {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let name: Ident = input.parse()?;
+        Ok(Instruction::NoOperand(name))
+    }
+}
+
+impl Parse for Instruction<ATNT> {
     fn parse(input: ParseStream) -> Result<Self> {
         let name: Ident = input.parse()?;
         Ok(Instruction::NoOperand(name))
@@ -16,23 +32,26 @@ impl Parse for Instruction {
 }
 
 
-impl Instruction
+
+
+impl<T> Instruction<T>
 {
     pub fn assemble(&self, assembler: &mut CodeAssembler)
     {
         match self {
             Instruction::NoOperand(ident) => assemble_nooperand(assembler, &format!("{ident}").to_lowercase()),
+            Instruction::_Phantom(_) => unreachable!()
         }
     }
-
-
 }
+
 
 fn assemble_nooperand(assembler: &mut CodeAssembler, instr: &str)
 {
     match instr
     {
         "nop" => assembler.nop(),
+        "ret" => assembler.ret(),
         _ => todo!()
     }.unwrap()
 }
--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,27 +1,33 @@
 extern crate proc_macro;
 
 mod instruction;
+mod register;
+
+use std::marker::PhantomData;
 
 use iced_x86::{code_asm::CodeAssembler};
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Literal};
 use proc_macro_error::proc_macro_error;
-use syn::{parse::{Parse, ParseStream, Result}, parse_macro_input, Token};
+use syn::{parse::{Parse, ParseStream, Result}, parse_macro_input, Token, token::In};
 use instruction::Instruction;
 
+struct ATNT;
+struct INTEL;
 
 
 #[derive(Debug)]
-struct Asm {
+struct Asm<T> {
+    _phantom: PhantomData<T>,
     bitness: u32,
-    instructions: Vec<Instruction>,
+    instructions: Vec<Instruction<T>>,
 }
 
 
 #[proc_macro_error]
 #[proc_macro]
 pub fn asm_intel(input: TokenStream) -> TokenStream {
-    let parsed = parse_macro_input!(input as Asm);
+    let parsed = parse_macro_input!(input as Asm<INTEL>);
     let data = parsed.serialize();
     //TODO: Figure out nicer way to convert Vec to const byte array
     let mut result = String::new();
@@ -40,7 +46,7 @@ pub fn asm_intel(input: TokenStream) -> TokenStream {
     result.parse().unwrap()
 }
 
-impl Asm
+impl<T> Asm<T>
 {
     fn serialize(&self) -> Vec<u8>
     {
@@ -56,16 +62,17 @@ impl Asm
 
 
 
-impl Parse for Asm {
+impl<T> Parse for Asm<T>
+where instruction::Instruction<T>:Parse {
     fn parse(input: ParseStream) -> Result<Self> {
         let mut instructions = Vec::new();
         let bitness: Literal = input.parse()?;
         let _ = input.parse::<Token![,]>()?;
         let bitness = format!("{bitness}").parse().unwrap();
         while !input.is_empty() {
-            let tag: Instruction = input.parse()?;
+            let tag: Instruction<T> = input.parse()?;
             instructions.push(tag);
         }
-        Ok(Asm{bitness, instructions})
+        Ok(Asm{bitness, instructions, _phantom: PhantomData})
     }
 }
