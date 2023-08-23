b6c809d8ba537e262372c3de574db2cb9f135154
JPaja <jovan.pavlovic99@gmail.com>
2023-08-23 17:04:39 +0200

    Add Missing read functions

--- src/pipe/io/read.rs
+++ src/pipe/io/read.rs
@@ -1,4 +1,4 @@
-use std::{io::Read, result};
+use std::result;
 
 use super::*;
 use bytes::BufMut;
@@ -30,19 +30,23 @@ impl<R: AsyncRead + Unpin, W: AsyncWrite + Unpin> Pipe<R, W> {
     async_impl_method!(read_to_end, usize, (buf: &mut Vec<u8>), (buf));
     async_impl_method!(read_to_string, usize, (buf: &mut String), (buf));
     async_impl_method!(read_u8, u8, (), ());
-    async_impl_method!(read_i8, i8, (), ());
     async_impl_method!(read_u16, u16, (), ());
     async_impl_method!(read_u32, u32, (), ());
     async_impl_method!(read_u64, u64, (), ());
+    async_impl_method!(read_u128, u128, (), ());
+    async_impl_method!(read_i8, i8, (), ());
     async_impl_method!(read_i16, i16, (), ());
     async_impl_method!(read_i32, i32, (), ());
     async_impl_method!(read_i64, i64, (), ());
+    async_impl_method!(read_i128, i128, (), ());
     async_impl_method!(read_u16_le, u16, (), ());
     async_impl_method!(read_u32_le, u32, (), ());
     async_impl_method!(read_u64_le, u64, (), ());
+    async_impl_method!(read_u128_le, u128, (), ());
     async_impl_method!(read_i16_le, i16, (), ());
     async_impl_method!(read_i32_le, i32, (), ());
     async_impl_method!(read_i64_le, i64, (), ());
+    async_impl_method!(read_i128_le, i128, (), ());
     async_impl_method!(read_f32, f32, (), ());
     async_impl_method!(read_f64, f64, (), ());
 }
