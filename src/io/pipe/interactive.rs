9c87eecff8eb35ea559f413de029e63ff7462fd7
JPaja <jovan.pavlovic99@gmail.com>
2024-01-02 23:22:07 +0100

    Add base for non blocking stdio shell mode

--- src/io/pipe/interactive.rs
+++ src/io/pipe/interactive.rs
@@ -1,11 +1,23 @@
 use crate::io::{CacheReader, HasCache, HasTimeout, Pipe, PipeReadExt, PipeWriteExt, TimeoutReader, TimeoutWriter};
 use std::io::ErrorKind::TimedOut;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::atomic::{AtomicBool};
 use std::sync::Arc;
 use tokio::io::{self, AsyncBufReadExt, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, BufReader};
-use tokio::join;
+use tokio::{join, task};
 use std::borrow::BorrowMut;
+use std::cmp::Ordering;
+use std::ptr::write;
 use std::time::Duration;
+use ascii::{AsciiChar, AsciiString};
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tokio::sync::mpsc::error::TryRecvError;
+use tokio::time::sleep;
+use crossterm::*;
+use crossterm::cursor::{DisableBlinking, position, RestorePosition, SavePosition};
+use crossterm::terminal::*;
+use crossterm::cursor::*;
+use crossterm::event::KeyEventKind;
+use crossterm::style::*;
 
 impl<T, R, W> PipeInteractiveExt<R, W> for T
 where
@@ -32,66 +44,136 @@ where
 }
 
 //TODO: Add error messages if read_task or write_task fail
-async fn read_task<R>(reader: &mut R, stop_signal: Arc<AtomicBool>)
+async fn read_task<R>(reader: &mut R, stop_signal: Arc<AtomicBool>, sender: Sender<Vec<u8>>)
 where
-    R: AsyncRead + Unpin  + Send,
+    R: AsyncRead + Unpin + Send,
 {
-    let mut stdout = io::stdout();
-    stdout.set_read_timeout(Some(Duration::from_millis(100)));
     let mut buffer = [0; 1024];
     loop {
-        if stop_signal.load(Ordering::SeqCst) {
+        if stop_signal.load(std::sync::atomic::Ordering::SeqCst) {
             break;
         }
 
         let n = match reader.read(&mut buffer).await {
             Ok(n) if n == 0 => continue,
             Ok(n) => n,
-            Err(e) if e.kind() == TimedOut => continue,
+            Err(e) if e.kind() == TimedOut => {
+                continue
+            },
             Err(_) => {
-                stop_signal.store(true, Ordering::SeqCst);
+                stop_signal.store(true, std::sync::atomic::Ordering::SeqCst);
                 break;
             }
         };
-        match stdout.write_all(&buffer[..n]).await {
+
+        match sender.send(buffer[..n].to_vec()).await {
             Ok(_) => {}
             Err(_) => {
-                stop_signal.store(true, Ordering::SeqCst);
+                stop_signal.store(true, std::sync::atomic::Ordering::SeqCst);
                 break;
             }
         }
     }
 }
 
-async fn write_task<W>(writer: &mut W, stop_signal: Arc<AtomicBool>)
+async fn write_task<W>(writer: &mut W, stop_signal: Arc<AtomicBool>, mut receiver: Receiver<Vec<u8>>)
 where
     W: AsyncWrite + Unpin + Send,
 {
-    let stdin = io::stdin();
-    let reader = BufReader::new(stdin);
-    let mut lines = reader.lines();
+    execute!(std::io::stdout(), SavePosition);
 
+    let mut stdout = io::stdout();
+
+    let mut cache = String::new();
     loop {
-        if stop_signal.load(Ordering::SeqCst) {
+        if stop_signal.load(std::sync::atomic::Ordering::SeqCst) {
             break;
         }
 
-        let mut line = match lines.next_line().await {
-            Err(_) | Ok(None) => {
-                stop_signal.store(true, Ordering::SeqCst);
+        match receiver.try_recv()
+        {
+            Ok(data) => {
+                println!("Received");
+                match stdout.write_all(&data).await
+                {
+                    Ok(_) => {
+                        let _ = stdout.flush().await;
+                        execute!(std::io::stdout(), SavePosition);
+                    }
+                    Err(_) => {
+                        stop_signal.store(true, std::sync::atomic::Ordering::SeqCst);
+                        break;
+                    }
+                }
+            },
+            Err(TryRecvError::Empty) => {},
+            Err(TryRecvError::Disconnected) => {
+                stop_signal.store(true, std::sync::atomic::Ordering::SeqCst);
                 break;
-            }
-            Ok(Some(line)) => line,
-        };
-        line.push('\n');
-        match writer.write(&line.as_bytes()).await {
-            Ok(_) => {}
-            Err(_) => {
-                stop_signal.store(true, Ordering::SeqCst);
-                break;
+            },
+        }
+
+        if let Ok(true) = event::poll(Duration::from_millis(0)) {
+            match event::read() {
+                Ok(event::Event::Key(key_event))  if key_event.kind == KeyEventKind::Press => {
+                   handle_key_event(key_event, &mut cache, writer).await;
+                },
+                Ok(event::Event::Resize(width, height)) => {},
+                _ => {},
             }
         }
     }
+}
+
+async fn clear_stdout()
+{
+    let (x,y) = cursor::position().unwrap();
+    execute!(std::io::stdout(), RestorePosition).unwrap();
+
+    let (old_x,old_y) = cursor::position().unwrap();
+
+    if y >= old_y
+    {
+        execute!(std::io::stdout(), Clear(ClearType::UntilNewLine)).unwrap();
+        for line in old_y + 1..=y {
+            execute!(std::io::stdout(), MoveTo(0, line), Clear(ClearType::CurrentLine)).unwrap();
+        }
+        execute!(std::io::stdout(), MoveTo(old_x, old_y)).unwrap();
+    }
+
+    execute!(std::io::stdout(), SavePosition).unwrap();
+}
+
+async fn handle_key_event<W>(key_event: event::KeyEvent, cache: &mut String, writer: &mut W)
+    where
+        W: AsyncWrite + Unpin + Send,
+{
+    clear_stdout().await;
+    match key_event.code {
+        event::KeyCode::Char(c) if c.is_ascii() => {
+            cache.push(c);
+        },
+        event::KeyCode::Backspace => {
+            let _ = cache.pop();
+        },
+        event::KeyCode::Enter => {
+            writer.write(cache.as_bytes()).await.unwrap();
+            let _ = writer.flush().await;
+            cache.clear();
+        },
+        event::KeyCode::Left => {
+            // Move cursor left...
+        },
+        event::KeyCode::Right => {
+            // Move cursor right...
+        },
+        event::KeyCode::Delete => {
+            // Handle delete...
+        }
+        _ => {}
+    }
+
+    execute!(std::io::stdout(), Print(cache));
 }
 
 pub trait PipeInteractiveExt<R, W>: AsyncReadWriteSplit<R, W> + Unpin
@@ -104,13 +186,33 @@ where
 
         let stop_signal = Arc::new(AtomicBool::new(false));
 
-        let reader = unsafe { &mut *reader_ptr };
-        let writer = unsafe { &mut *writer_ptr };
+        let (rx,tx) = channel(100);
 
-        let reader_task = read_task(reader, stop_signal.clone());
-        let writer_task = write_task(writer, stop_signal);
+        let reader_ptr = reader_ptr as usize;
+        let writer_ptr = writer_ptr as usize;
+
+
+        terminal::enable_raw_mode().unwrap();
+        execute!(std::io::stdout(), EnableBlinking);
+
+        let read_stop_signal = stop_signal.clone();
+        let reader_task = tokio::spawn(async move {
+            let reader_ptr = unsafe { reader_ptr as *mut R};
+            let reader = unsafe { &mut *reader_ptr };
+            read_task(reader, read_stop_signal.clone(), rx).await
+        });
+
+        let writer_task = tokio::spawn(async move {
+            let writer_ptr = unsafe { writer_ptr as *mut W};
+            let writer = unsafe { &mut *writer_ptr };
+            write_task(writer, stop_signal, tx).await
+        });
 
         join!(reader_task, writer_task);
+
+        execute!(std::io::stdout(), DisableBlinking);
+
+        terminal::disable_raw_mode().unwrap();
 
         Ok(())
     }
