b4b7e2f2681b07c1c1bea6e632c959469f56bf1c
JPaja <jovan.pavlovic99@gmail.com>
2024-01-09 23:49:35 +0100

    Fix: MergedAsyncReader

--- src/io/merge/mod.rs
+++ src/io/merge/mod.rs
@@ -1,13 +1,19 @@
 use std::task::Poll;
+use pin_project_lite::pin_project;
 use tokio::io::*;
 
 //TODO: AutoGenerate MergedOutput for up to ten args
 
-#[derive(Debug)]
-pub struct MergedAsyncReader<R1, R2> {
-    pub stream0: R1,
-    pub stream1: R2,
+pin_project! {
+    #[derive(Debug)]
+    pub struct MergedAsyncReader<R1, R2> {
+        #[pin]
+        pub stream0: R1,
+        #[pin]
+        pub stream1: R2,
+    }
 }
+
 
 impl<R1, R2> MergedAsyncReader<R1, R2> {
     pub fn new(stream0: R1, stream1: R2) -> MergedAsyncReader<R1, R2> {
@@ -21,11 +27,10 @@ impl<R1: AsyncRead + Unpin, R2: AsyncRead + Unpin> AsyncRead for MergedAsyncRead
         cx: &mut std::task::Context<'_>,
         buf: &mut ReadBuf<'_>,
     ) -> Poll<Result<()>> {
-        let stream0_bytes = std::pin::Pin::new(&mut self.stream0).poll_read(cx, buf);
-        match stream0_bytes {
-            Poll::Ready(Ok(r)) if buf.capacity() == buf.filled().len() => Poll::Ready(Ok(r)),
-            Poll::Ready(Err(e)) => Poll::Ready(Err(e)),
-            _ => std::pin::Pin::new(&mut self.stream1).poll_read(cx, buf),
+        let me = self.project();
+        match me.stream0.poll_read(cx, buf) {
+            Poll::Ready(r) => Poll::Ready(r),
+            Poll::Pending => me.stream1.poll_read(cx, buf),
         }
     }
 }
